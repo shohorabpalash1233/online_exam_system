a3cec24842003425c4e7999515a5a06fd1a2b8c5
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-14 23:06:28 +0600

    Admin and User controller created

---
+++ classes/User.php
@@ -0,0 +1,53 @@
+<?php
+	/**
+	* User Class
+	*/
+
+    $filepath = realpath(dirname(__FILE__));
+	include_once ($filepath.'/../lib/Database.php');
+	include_once ($filepath.'/../helpers/Format.php');
+
+	class User
+	{	
+		private $db;
+		private $fm;
+		
+		function __construct()
+		{
+			$this->db = new Database();
+			$this->fm = new Format();
+		}
+
+
+
+		public function getAdminData($data){
+			$adminUser = $this->fm->validation($data['adminUser']);
+			$adminPass = $this->fm->validation($data['adminPass']);
+
+			$adminUser = mysqli_real_escape_string($this->db->link, $adminUser);
+			$adminPass = mysqli_real_escape_string($this->db->link, md5($adminPass));
+
+		}
+
+		public function getAllUser(){
+			$query = "SELECT * FROM tbl_user ORDER BY userId DESC";
+			$result = $this->db->select($query);
+			return $result;
+		}
+
+		public function disableUser($disable){
+			$query = "UPDATE tbl_user
+					  SET
+					  status = '1' 
+					  WHERE userId = '$disable' ";
+			$update = $this->db->update($query);
+			if ($update) {
+				$msg = "<span class='success'>User disabled!</span>";
+				return $msg;
+			}else{
+				$msg = "<span class='error'>User Not disabled!</span>";
+				return $msg;
+			}
+		}
+	}
+?>
