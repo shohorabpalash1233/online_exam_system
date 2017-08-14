a28295f7c2447aad36190ee53b98180d933de177
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-14 23:01:03 +0600

    login system created

--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,15 @@
 <?php 
     $filepath = realpath(dirname(__FILE__));
 	include_once ($filepath.'/inc/loginheader.php');
-?>
+	include_once ($filepath.'/../classes/Admin.php');
 
+	$admin = new Admin();
+?>
+<?php
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$adminData = $admin->getAdminData($_POST);
+	}
+?>
 <div class="main">
 <h1>Admin Login</h1>
 <div class="adminlogin">
@@ -10,15 +17,24 @@
 		<table>
 			<tr>
 				<td>Username</td>
-				<td><input type="text" name="username"/></td>
+				<td><input type="text" name="adminUser" required="required" /></td>
 			</tr>
 			<tr>
 				<td>Password</td>
-				<td><input type="password" name="password"/></td>
+				<td><input type="password" name="adminPass" required="required" /></td>
 			</tr>
 			<tr>
 				<td></td>
 				<td><input type="submit" name="login" value="Login"/></td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<?php
+						if (isset($adminData)) {
+							echo $adminData;
+						}
+					?>
+				</td>
 			</tr>
 		</table>
 	</from>
