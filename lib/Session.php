21f1edec0b93b97dd1ff9f53e3b43018225c50a7
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-14 23:05:37 +0600

    Session controller file modified

--- lib/Session.php
+++ lib/Session.php
@@ -16,24 +16,39 @@ class Session{
 	 	}
 	 }
 
+	 public static function checkAdminSession(){
+	 	self::init();
+	 	if (self::get("adminLogin") == false) {
+	 		self::destroy();
+	 		header("Location: login.php");
+	 	}
+	 }
+
+	 public static function checkAdminLogin(){
+	 	self::init();
+	 	if (self::get("adminLogin") == true) {
+	 		header("Location: index.php");
+	 	}
+	 }
+
 	 public static function checkSession(){
 	 	self::init();
 	 	if (self::get("login") == false) {
 	 		self::destroy();
-	 		header("Location:login.php");
+	 		header("Location: login.php");
 	 	}
 	 }
 
 	 public static function checkLogin(){
 	 	self::init();
 	 	if (self::get("login") == true) {
-	 		header("Location:index.php");
+	 		header("Location: index.php");
 	 	}
 	 }
 
 	 public static function destroy(){
 	 	session_destroy();
-	 	header("Location:login.php");
+	 	session_unset();
 	 }
 }
 
