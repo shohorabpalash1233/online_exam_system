0717fd26d8da3a450320d7037fb63c4b51235086
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-14 23:00:32 +0600

    index file complete setup

--- admin/index.php
+++ admin/index.php
@@ -2,13 +2,29 @@
     $filepath = realpath(dirname(__FILE__));
 	include_once ($filepath.'/inc/header.php');
 ?>
-<?php
-  // Session::checkLogin();
-?>
-
+<style type="text/css" media="screen">
+	.adminpanel{
+		width: 500px;
+		color: #999;
+		margin: 30px auto 0;
+		padding: 50px;
+		border: 1px solid #ddd;
+	}
+	.adminpanel h2{
+		font-size: 30px;
+		text-align: center;
+	}
+	.adminpanel p{
+		font-size: 14px;
+		text-align: center;
+	}
+</style>	
 <div class="main">
 <h1>Admin Panel</h1>
-
+<div class="adminpanel">
+	<h2>Welcome To Admin Panel</h2>
+	<p>You can manage your users and online exam system here...</p>
+</div>
 
 
 	
