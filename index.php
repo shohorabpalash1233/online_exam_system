37e8e423f5d3297f44515eb64182a0a67236d962
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-17 23:53:13 +0600

    login validation added

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php include 'inc/header.php'; ?>
+<?php
+	Session::checkLogin();
+?>
 <div class="main">
 <h1>Online Exam System - User Login</h1>
 	<div class="segment" style="margin-right:30px;">
@@ -8,25 +11,28 @@
 	<form action="" method="post">
 		<table class="tbl">    
 			 <tr>
-			   <td>Username</td>
-			   <td><input name="username" type="text"></td>
+			   <td>Email</td>
+			   <td><input name="email" type="text" id="email"></td>
 			 </tr>
 			 <tr>
 			   <td>Password </td>
-			   <td><input name="password" type="password"></td>
+			   <td><input name="password" type="password" id="password"></td>
 			 </tr>
 			 
 			  <tr>
 			  <td></td>
-			   <td><input type="submit" name="login" value="Login">
+			   <td><input type="submit" name="login" id="loginsubmit" value="Login">
 			   </td>
 			 </tr>
        </table>
 	   </form>
 	   <p>New User ? <a href="register.php">Signup</a> Free</p>
+	   <span class="empty" style="display: none;">Field must not be empty! </span>
+	   <span class="error" style="display: none;">Email and password not matched! </span>
+	   <span class="disable" style="display: none;">User ID disabled! </span>
 	</div>
 
-
+	
 	
 </div>
 <?php include 'inc/footer.php'; ?>
