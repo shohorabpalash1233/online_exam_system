27e5585229131d97b83bfbf72a32d569be42835d
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-18 23:45:13 +0600

    main.js modfied

--- js/main.js
+++ js/main.js
@@ -1,54 +1,60 @@
-$(function(){
+$(function() {
+
 	//For User Registration
 	$("#rsubmit").click(function() {
-		var name 		= $("#name").val();
-		var username 	= $("#username").val();
-		var password 	= $("#password").val();
-		var email 		= $("#email").val();
 
-		var dataString = 'name='+name+'&username='+username+'&password='+password+'&email='+email;
+		var name = $("#name").val();
+		var username = $("#username").val();
+		var password = $("#password").val();
+		var email = $("#email").val();
+
+		var dataString = 'name=' + name + '&username=' + username + '&password=' + password + '&email=' + email;
 
 		$.ajax({
 			type: 'POST',
 			url: "getregister.php",
 			data: dataString,
-			success: function(data){
+			success: function(data) {
 				$("#state").html(data);
 			}
 		});
 		return false;
 	});
 
-		//For User Login
+
+	//For User Login
 	$("#loginsubmit").click(function() {
 
-		var email 		= $("#email").val();
-		var password 	= $("#password").val();
-		
-		var dataString = 'email='+email+'&password='+password;
+		var email = $("#email").val();
+		var password = $("#password").val();
+
+		var dataString = 'email=' + email + '&password=' + password;
 
 		$.ajax({
 			type: 'POST',
 			url: "getlogin.php",
 			data: dataString,
-			success: function(data){
+			success: function(data) {
 				if ($.trim(data) == "empty") {
 
 					$(".empty").show();
-					$(".error").hide();
-					$(".disable").hide();
+					setTimeout(function() {
+						$(".empty").fadeOut();
+					}, 1000);
 
 				} else if ($.trim(data) == "disable") {
 
-					$(".empty").hide();
-					$(".error").hide();
 					$(".disable").show();
+					setTimeout(function() {
+						$(".disable").fadeOut();
+					}, 1000);
 
 				} else if ($.trim(data) == "error") {
 
-					$(".empty").hide();
 					$(".error").show();
-					$(".disable").hide();
+					setTimeout(function() {
+						$(".error").fadeOut();
+					}, 1000);
 
 				} else {
 					window.location = "exam.php";
