df1b4beb8e51830963091f432c2f59071079eae4
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-18 23:42:06 +0600

    test question shown

--- test.php
+++ test.php
@@ -1,40 +1,49 @@
 <?php include 'inc/header.php'; ?>
+<?php
+	Session::checkSession();
+	if (isset($_GET['q'])) {
+		$number = $_GET['q'];
+	}else{
+		header("Locaion: exam.php");
+	}
+	$total = $exam->getTotalRows();
+	$question = $exam->getQuestionByNumber($number);
+?>
+
+<?php
+	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+		$process = $pro->processData($_POST);
+	}
+?>
 <div class="main">
-<h1>Welcome to Online Exam</h1>
+<h1>Question <?php echo $question['quesNo']; ?> of <?php echo $total; ?></h1>
 	<div class="test">
 		<form method="post" action="">
 		<table> 
 			<tr>
 				<td colspan="2">
-				 <h3>Que 1: What is the first event that will be triggered in the from?</h3>
+				 <h3>Que <?php echo $question['quesNo']; ?>: <?php echo $question['ques']; ?></h3>
 				</td>
 			</tr>
-
+			<?php
+				$answer = $exam->getAnswer($number);
+				if ($answer) {
+					while ($result = $answer->fetch_assoc()) {	
+			?>
 			<tr>
 				<td>
-				 <input type="radio" name="ans1"/>Load
+				 <input type="radio" name="ans" value="<?php echo $result['id']; ?>" />
+				 	<?php echo $result['ans']; ?>
 				</td>
 			</tr>
-			<tr>
-				<td> 
-				<input type="radio" name="ans2"/>GotFocus
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="radio" name="ans3"/>Instance
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="radio" name="ans4"/>Initialize
-				</td>
-			</tr>
-
+			<?php
+				}
+					}
+			?>
 			<tr>
 			  <td>
 				<input type="submit" name="submit" value="Next Question"/>
-				<input type="hidden" name="number"/>
+				<input type="hidden" name="number" value="<?php echo $number; ?>" />
 			</td>
 			</tr>
 			
